96cdec465b420931a8959a5d72baedb2a07d1623
parkergeorge13 <parkergeorge13@gmail.com>
2023-02-13 18:46:12 -0700

    Add project files.

---
+++ Mission06_pdg13/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission06_pdg13.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_pdg13.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieDatabaseContext blahContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, MovieDatabaseContext someName)
+        {
+            _logger = logger;
+            blahContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Movies(ApplicationResponse ar)
+        {
+            blahContext.Add(ar);
+            blahContext.SaveChanges();
+            return View("Confirmation", ar);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission06_pdg13/Models/MovieDatabaseContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_pdg13.Models
+{
+    public class MovieDatabaseContext : DbContext
+    {
+        //Constructor
+        public MovieDatabaseContext (DbContextOptions<MovieDatabaseContext> options) : base (options)
+        {
+            //Leave blank for now
+        }
+
+        public DbSet<ApplicationResponse> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<ApplicationResponse>().HasData(
+                new ApplicationResponse
+                {
+                    ApplicationID = 1,
+                    Category = "Action/Adventure",
+                    Title = "Thor Ragnarok",
+                    Year = 2017,
+                    Director = "Taika Waititi",
+                    Rating = "PG-13"
+                },
+                new ApplicationResponse
+                {
+                    ApplicationID = 2,
+                    Category = "Sci-Fi",
+                    Title = "Tenet",
+                    Year = 2020,
+                    Director = "Christopher Nolan",
+                    Rating = "PG-13"
+                },
+                new ApplicationResponse
+                    {
+                        ApplicationID = 3,
+                        Category = "Comedy",
+                        Title = "The Lego Batman Movie",
+                        Year = 2017,
+                        Director = "Chris McKay",
+                        Rating = "PG"
+                    }
+            );
+        }
+    }
+}
